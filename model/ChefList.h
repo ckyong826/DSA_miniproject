615e4d9fdc343f96d0bd4f1dcadf144d7441bd36
Elvis Chang Feng Jie <elvis.15chang@gmail.com>
2024-01-23 20:26:05 +0800

    header

---
+++ model/ChefList.h
@@ -0,0 +1,27 @@
+#ifndef CHEFLIST.H
+#define CHEFLIST_H
+
+#include "Chef.h"
+using namespace std;
+
+class ChefNode {
+public:
+    Chef chef;
+    ChefNode* next;
+
+    ChefNode();
+    ChefNode(Chef chef);
+};
+
+class ChefList {
+private:
+    ChefNode* head;
+
+public:
+    ChefList();
+    void addChef(string chefID, string chefName, string chefPassword);
+    void deleteChef(string chefID);
+    Chef* findChef(string chefID);
+};
+
+#endif
